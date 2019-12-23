ab5ca271ce28b40616c32553e29df486407b1022
Rajat Gupta <rajat.11605708@lpu.in>
2019-12-23 16:07:26 +0530

    Print Functionality in fmt package

---
+++ 01-Basics/fmt-package-print.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var num = 42
+
+func main() {
+	fmt.Println("Value of num : ", num)
+	fmt.Printf("Type of num : %T\n", num)
+	fmt.Printf("Binary Notification of num : %b\n", num)
+	fmt.Printf("Hexadecimal Notification of num : %x\n", num)
+
+}
